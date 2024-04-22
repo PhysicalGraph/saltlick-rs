943b5187932ee86fc752a70099a124d0fa84185c
Judge Maygarden <judge.maygarden@smartthings.com>
2024-04-21 19:20:25 -0500

    replace sodiumoxide with crypto_box and crypto_secretstream

--- src/key.rs
+++ src/key.rs
@@ -7,9 +7,9 @@
 // except according to those terms.
 
 use crate::error::{SaltlickError, SaltlickKeyIoError};
+use crypto_box::{aead::OsRng, PublicKey as SodiumPublicKey, SecretKey as SodiumSecretKey};
 use lazy_static::lazy_static;
 use simple_asn1::{self, ASN1Block, ASN1Class, BigInt, FromASN1, ToASN1, OID};
-use sodiumoxide::crypto::box_::{PublicKey as SodiumPublicKey, SecretKey as SodiumSecretKey};
 use std::{
     fs::{File, OpenOptions},
     io::{Read, Write},
@@ -17,7 +17,7 @@ use std::{
     str,
 };
 
-pub use sodiumoxide::crypto::box_::{self, PUBLICKEYBYTES, SECRETKEYBYTES};
+pub use crypto_box::{KEY_SIZE as PUBLICKEYBYTES, KEY_SIZE as SECRETKEYBYTES};
 
 lazy_static! {
     static ref CURVE25519_OID: OID = simple_asn1::oid!(1, 3, 101, 110);
@@ -42,7 +42,7 @@ impl PublicKey {
     /// by libsodium 1.x.
     pub fn from_raw_curve25519(bytes: &[u8]) -> Result<PublicKey, SaltlickError> {
         let sodium_key =
-            SodiumPublicKey::from_slice(bytes).ok_or(SaltlickError::IncorrectKeyLength)?;
+            SodiumPublicKey::from_slice(bytes).map_err(|_| SaltlickError::IncorrectKeyLength)?;
         Ok(PublicKey { inner: sodium_key })
     }
 
@@ -91,7 +91,7 @@ impl ToASN1 for PublicKey {
         public_key_asn1.push(ASN1Block::BitString(
             0,
             PUBLICKEYBYTES * 8,
-            Vec::from(&self.inner[..]),
+            Vec::from(self.inner.as_bytes()),
         ));
         Ok(vec![ASN1Block::Sequence(0, public_key_asn1)])
     }
@@ -171,7 +171,7 @@ impl SecretKey {
     /// libsodium 1.x.
     pub fn from_raw_curve25519(bytes: &[u8]) -> Result<SecretKey, SaltlickError> {
         let sodium_key =
-            SodiumSecretKey::from_slice(bytes).ok_or(SaltlickError::IncorrectKeyLength)?;
+            SodiumSecretKey::from_slice(bytes).map_err(|_| SaltlickError::IncorrectKeyLength)?;
         Ok(SecretKey { inner: sodium_key })
     }
 
@@ -219,7 +219,7 @@ impl ToASN1 for SecretKey {
         private_key_asn1.push(ASN1Block::Integer(0, BigInt::from(0u8)));
         private_key_asn1.extend(Curve25519Algorithm.to_asn1()?);
         let wrapped_key =
-            simple_asn1::to_der(&ASN1Block::OctetString(0, Vec::from(&self.inner[..])))?;
+            simple_asn1::to_der(&ASN1Block::OctetString(0, Vec::from(self.inner.to_bytes())))?;
         private_key_asn1.push(ASN1Block::OctetString(0, wrapped_key));
         Ok(vec![ASN1Block::Sequence(0, private_key_asn1)])
     }
@@ -259,8 +259,8 @@ impl FromASN1 for SecretKey {
             Some(ASN1Block::OctetString(_, secret_key)) => secret_key,
             _ => return Err(SaltlickError::InvalidKeyFormat),
         };
-        let sodium_secret_key =
-            SodiumSecretKey::from_slice(&secret_key[..]).ok_or(SaltlickError::InvalidKeyFormat)?;
+        let sodium_secret_key = SodiumSecretKey::from_slice(&secret_key[..])
+            .map_err(|_| SaltlickError::InvalidKeyFormat)?;
 
         Ok((
             SecretKey {
@@ -273,7 +273,8 @@ impl FromASN1 for SecretKey {
 
 /// Create a new saltlick keypair.
 pub fn gen_keypair() -> (PublicKey, SecretKey) {
-    let (raw_public, raw_secret) = box_::gen_keypair();
+    let raw_secret = SodiumSecretKey::generate(&mut OsRng);
+    let raw_public = raw_secret.public_key();
     (
         PublicKey { inner: raw_public },
         SecretKey { inner: raw_secret },
